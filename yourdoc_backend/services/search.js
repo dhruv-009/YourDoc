c5c838d52c3f70134b19a55fbeb3078c146acc6f
vasumakwana <vasumakwana02@gmail.com>
2023-03-06 01:17:35 -0400

    Search: created api for search by pincode.

--- yourdoc_backend/services/search.js
+++ yourdoc_backend/services/search.js
@@ -7,18 +7,17 @@ const { v4: uuid } = require('uuid');
 
 async function getDocSpec(spec) {
   const result = await db.query(
-      `SELECT * FROM doctor WHERE specialization = '${spec}'`
+      `SELECT * FROM doctor WHERE specialization = '${spec}' and is_approved = 1`
   );
   console.log(result);
   return {
     result
   }
 }
-
 
 async function searchName(docName) {
   const result = await db.query(
-      `SELECT * FROM doctor WHERE name = '${docName}'`
+      `SELECT * FROM doctor WHERE name = '${docName}' and is_approved = 1`
   );
   console.log(result);
   return {
@@ -26,9 +25,18 @@ async function searchName(docName) {
   }
 }
 
-// getDocSpec('Dentist')
+async function searchPinCode(pinCode) {
+    const result = await db.query(
+        `SELECT * FROM doctor INNER JOIN user ON address = '${pinCode}' and is_approved = 1;`
+    );
+    console.log(result);
+    return {
+        result
+    }
+}
 
 module.exports = {
-  searchName,
-  getDocSpec
+    searchName,
+    getDocSpec,
+    searchPinCode
 }
