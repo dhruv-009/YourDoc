3c9841e3fe15f50e73b6285412fed918278b3eb7
Priyank Patel <pr371749@dal.ca>
2023-03-08 14:01:35 -0400

    Admin backend with accept and reject feature

--- yourdoc_backend/app.js
+++ yourdoc_backend/app.js
@@ -8,12 +8,9 @@ var userRouter = require('./routes/user');
 var doctorRouter = require('./routes/doctor');
 var appointmentRouter = require('./routes/appointment');
 var patientRouter = require('./routes/patient');
-<<<<<<< HEAD
 var adminRouter = require('./routes/admin');
 var cors = require('cors');
-=======
 var searchRouter = require('./routes/search');
->>>>>>> development
 var app = express();
 
 app.use(logger('dev'));
@@ -27,10 +24,7 @@ app.use('/user', userRouter);
 app.use('/doctor', doctorRouter);
 app.use('/patient', patientRouter);
 app.use('/appointment', appointmentRouter);
-<<<<<<< HEAD
 app.use('/admin', adminRouter);
-=======
 app.use('/search', searchRouter);
->>>>>>> development
 
 module.exports = app;
