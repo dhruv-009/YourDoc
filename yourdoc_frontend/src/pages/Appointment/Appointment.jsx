8dac7a5d756bfc79625570e4051d387936dfb24c
Anubhooti Pathak <an499564@dal.ca>
2023-03-19 13:00:08 -0400

    appointment from api

--- yourdoc_frontend/src/pages/Appointment/Appointment.jsx
+++ yourdoc_frontend/src/pages/Appointment/Appointment.jsx
@@ -5,18 +5,31 @@ import { TimeSlots } from './TImeSlots';
 import { DaySelector } from './DaySelector';
 import { DoctorCard } from './DoctorCard';
 
-const BASE_URL = 'http://localhost:3000'
-function useAppointment(doctorUserId) {
+const BASE_URL = 'http://localhost:3000';
+const tempDoctorId = "1";
 
-  useEffect(async () => {
-    const a = await fetch(BASE_URL + '/').then(j => j.json());
+function useAppointment(doctorUserId) {
+  const [availableSlots, setAvailableSlots] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { data } = await fetch(BASE_URL + `/availability?doctor_id=${tempDoctorId}`).then(j => j.json())
+      setAvailableSlots(data);
+    })()
   }, []);
+
+  return { availableSlots };
 }
 
 export function Appointment() {
-  const availableSlots = [{ day: 'mon', from: "8:30", to: "12:30" }, { day: 'mon', from: "16:30", to: "21:30" }, { day: 'sun', from: "12:00", to: "15:30" }];
+  // const availableSlots = [{ day: 'mon', from: "08:30", to: "12:30" }, { day: 'mon', from: "16:30", to: "21:30" }, { day: 'sun', from: "12:00", to: "15:30" }];
+  const { availableSlots } = useAppointment();
   const [luxSelectedDay, setLuxSelectedDay] = useState();
+  const [selectedTimes, setSelectedTimes] = useState([]);
 
+  const handleSetSelectedDay = (day) => {
+    setLuxSelectedDay(day);
+    setSelectedTimes([]);
+  }
 
   return (
     <div>
@@ -28,10 +41,13 @@ export function Appointment() {
         <div className="flex-1 w-full max-w-screen-sm mx-auto flex-wrap">
           <div className="justify-center flex bg-white dark:bg-gray-900 shadow-md rounded-lg overflow-x-scroll mx-auto py-4 px-2 md:mx-12">
             {/* <button type="button" className="btnArrow">←</button> */}
-            <DaySelector luxSelectedDay={luxSelectedDay} setLuxSelectedDay={setLuxSelectedDay} />
+            <DaySelector luxSelectedDay={luxSelectedDay} setLuxSelectedDay={handleSetSelectedDay} />
             {/* <button type="button" className="btnArrow">→</button> */}
           </div>
-          <TimeSlots selectedDay={luxSelectedDay?.toFormat('ccc')} availableSlots={availableSlots} />
+          <TimeSlots selectedDay={luxSelectedDay?.toFormat('ccc')} availableSlots={availableSlots}
+            setSelectedTimes={setSelectedTimes} selectedTimes={selectedTimes}
+          />
+          <button type="button" className="btnTime" disabled={!selectedTimes.length}>Book Now</button>
         </div>
       </div>
     </div>
