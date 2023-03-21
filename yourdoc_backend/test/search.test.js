92de3a3fede77c4486a42850f8be2dfc329ea837
vasumakwana <vasumakwana02@gmail.com>
2023-03-21 11:57:54 -0300

    Test: test case returns approved doctors based on specialization

---
+++ yourdoc_backend/test/search.test.js
@@ -0,0 +1,25 @@
+const db = require('../services/db');
+const searchob = require('../services/search');
+
+
+describe('searchDocBySpec', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+  
+    test('returns approved doctors based on specialization', async () => {
+      const spec = 'cardiology';
+      const expectedResult = { result: { rows: [{id: 1, name: 'Dr. Smith', specialization: 'cardiology'}] }};
+  
+      const mockResult = { rows: [{id: 1, name: 'Dr. Smith', specialization: 'cardiology'}] };
+      const querySpy = jest.spyOn(db, 'query').mockResolvedValue(mockResult);
+  
+      const result = await searchob.getDocSpec(spec);
+  
+      expect(querySpy).toHaveBeenCalledTimes(1);
+      expect(querySpy).toHaveBeenCalledWith("SELECT * FROM doctor WHERE specialization = 'cardiology' and is_approved = 1");
+  
+      expect(result).toEqual(expectedResult);
+    });
+});
+  
