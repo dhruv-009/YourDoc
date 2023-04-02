bc05cf746acd3268b12324ac304d595a6e8bda86
Anubhooti Pathak <an499564@dal.ca>
2023-04-02 11:24:05 -0300

    Update helper.js

--- yourdoc_backend/helper.js
+++ yourdoc_backend/helper.js
@@ -2,20 +2,12 @@ function getOffset(currentPage = 1, listPerPage) {
     return (currentPage - 1) * [listPerPage];
 }
 
-// function emptyOrRows(rows) {
-//     if (!rows) {
-//         return [];
-//     }
-//     return rows;
-// }
-
 function emptyOrRows(rows) {
-    if (!rows || !Array.isArray(rows) || rows.length === 0) {
-      return [];
-    } else {
-      return rows;
+    if (!rows) {
+        return [];
     }
-  }
+    return rows;
+}
 
 module.exports = {
     getOffset,
