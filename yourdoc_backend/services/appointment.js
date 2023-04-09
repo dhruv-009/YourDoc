0430e5cd42a003a53aadb8992076fefd3a24a5bf
Dhruv Upadhyay <dhupadhyay2000@gmail.com>
2023-04-09 02:03:14 -0300

    fix: refactored the deleteA function to pass the test cases

--- yourdoc_backend/services/appointment.js
+++ yourdoc_backend/services/appointment.js
@@ -60,10 +60,13 @@ async function create(appointment) {
 }
 
 async function deleteA(appointmentId) {
+  if (!appointmentId || isNaN(parseInt(appointmentId))) {
+    throw new Error('Invalid input');
+  }
+
   const result = await db.query(
-    `DELETE FROM appointment
-    WHERE id='${appointmentId}'
-    `
+    `DELETE FROM appointment WHERE id= ?`,
+    [appointmentId]
   );
 
   let message = 'Error in deleting appointment';
